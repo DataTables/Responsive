DataTables/Responsive

commit ed1fdb5a85a33da6eb77b946e7388f36ede90a6f

Fix: `r-init responsive.details` when given as boolean false would result in an error.
- Fixes #61

js/dataTables.responsive.js
@@ -187,6 +187,7 @@ $.extend( Responsive.prototype, {
 
 		// Details handler
 		var details = this.c.details;
+
 		if ( details.type !== false ) {
 			that._detailsInit();
 
@@ -516,15 +517,18 @@ $.extend( Responsive.prototype, {
 	{
 		var that = this;
 		var dt = this.s.dt;
+		var details = this.c.details;
 
-		var res = this.c.details.display( row, update, function () {
-			return that.c.details.renderer(
-				dt, row[0], that._detailsObj(row[0])
-			);
-		} );
+		if ( details && details.type ) {
+			var res = details.display( row, update, function () {
+				return details.renderer(
+					dt, row[0], that._detailsObj(row[0])
+				);
+			} );
 
-		if ( res === true || res === false ) {
-			$(dt.table().node()).triggerHandler( 'responsive-display.dt', [dt, row, res, update] );
+			if ( res === true || res === false ) {
+				$(dt.table().node()).triggerHandler( 'responsive-display.dt', [dt, row, res, update] );
+			}
 		}
 	},
 

commit 10b94db94c41a2b005dcd37d412869cc393292c2

Dev fix: Correct error introduced in ed1fdb5a85a33
Fixes #69

js/dataTables.responsive.js
@@ -567,7 +567,7 @@ $.extend( Responsive.prototype, {
 
 		// Click handler to show / hide the details rows when they are available
 		$( dt.table().body() )
-			.on( 'click.dtr mousedown.dtr', selector, function () {
+			.on( 'click.dtr mousedown.dtr', selector, function (e) {
 				// If the table is not collapsed (i.e. there is no hidden columns)
 				// then take no action
 				if ( ! $(dt.table().node()).hasClass('collapsed' ) ) {