d1b59a40aa28566ac0d8e86c594124fa57b9d460
Allan Jardine <allan.jardine@sprymedia.co.uk>
2016-01-20 15:42:34 +0000

    Responsive 2.0.1 release

--- js/dataTables.responsive.js
+++ js/dataTables.responsive.js
@@ -1,11 +1,11 @@
-/*! Responsive 2.0.1-dev
+/*! Responsive 2.0.1
  * 2014-2015 SpryMedia Ltd - datatables.net/license
  */
 
 /**
  * @summary     Responsive
  * @description Responsive tables plug-in for DataTables
- * @version     2.0.1-dev
+ * @version     2.0.1
  * @file        dataTables.responsive.js
  * @author      SpryMedia Ltd (www.sprymedia.co.uk)
  * @contact     www.sprymedia.co.uk/contact
@@ -1159,7 +1159,7 @@ Api.register( 'responsive.hasHidden()', function () {
  * @name Responsive.version
  * @static
  */
-Responsive.version = '2.0.1-dev';
+Responsive.version = '2.0.1';
 
 
 $.fn.dataTable.Responsive = Responsive;
