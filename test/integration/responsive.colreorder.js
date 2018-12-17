fc42748bf6b09349fe7e5a987d7c3d0d902e5289
Colin Marks <colin@datatables.net>
2018-12-17 12:20:33 +0000

    DEV more unit tests

---
+++ test/integration/responsive.colreorder.js
@@ -0,0 +1,30 @@
+describe('Responsive - responsive.colreorder', function() {
+	dt.libs({
+		js: ['jquery', 'datatables', 'responsive', 'colreorder'],
+		css: ['datatables', 'responsive', 'colreorder']
+	});
+
+	let table;
+
+	dt.html('basic_wide');
+	it('Confirm initial setup', function() {
+		table = $('#example').DataTable({
+			responsive: true,
+			colReorder: true
+		});
+		expect($('thead th:visible').text()).toBe('First nameLast namePositionOfficeAgeStart dateSalaryExtn.');
+	});
+	it('Open a child', function() {
+		$('tbody tr:eq(2) td:eq(0)').click();
+		expect($('ul li:nth-child(1) span:eq(0)').text()).toBe('E-mail');
+		expect($('ul li:nth-child(2) span:eq(0)').text()).toBe('Line Manager First Name');
+		expect($('ul li:nth-child(3) span:eq(0)').text()).toBe('Line Manager Last Name');
+	});
+	// failing due to jira DD-757
+	// it('Move a row', function() {
+	// 	table.colReorder.move(0,10);
+	// 	//table.draw();
+	// 	expect($('ul li:nth-child(1) span:eq(0)').text()).toBe('E-mail');
+	// 	expect($('ul li:nth-child(3) span:eq(0)').text()).toBe('Line Manager Last Name');
+	// });
+});
