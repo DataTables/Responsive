2c9d441bec19c5f2a4f440b447b48b8bda708a7d
SandyDatatables <sandy@datatables.net>
2021-05-20 10:30:41 +0000

    dev: Remove console.logs from bulma js and change width of bulma examples so that they are initialised in a responsive state
    Jira Issue DD-1974

--- js/responsive.bulma.js
+++ js/responsive.bulma.js
@@ -53,39 +53,32 @@ var _modal = $(
 
 _display.modal = function ( options ) {
 	return function ( row, update, render ) {
-		console.log($.fn.modal)
-		// if ( ! $.fn.modal ) {
-		// 	_original( row, update, render );
-		// }
-		// else {
-			console.log("update", update)
-			if ( ! update ) {
-				if ( options && options.header ) {
-					var header = _modal.find('div.modal-header');
-					header.find('button').detach();
-					
-					header
-						.empty()
-						.append( '<h4 class="modal-title subtitle">'+options.header( row )+'</h4>' );
-				}
-
-				_modal.find( 'div.modal-body' )
+		if ( ! update ) {
+			if ( options && options.header ) {
+				var header = _modal.find('div.modal-header');
+				header.find('button').detach();
+				
+				header
 					.empty()
-					.append( render() );
-
-				_modal
-					.appendTo( 'body' )
-
-				_modal.addClass('is-active is-clipped');
-
-				$('.modal-close').one('click', function() {
-					_modal.removeClass('is-active is-clipped');
-				})
-				$('.modal-background').one('click', function() {
-					_modal.removeClass('is-active is-clipped');
-				})
+					.append( '<h4 class="modal-title subtitle">'+options.header( row )+'</h4>' );
 			}
-		// }
+
+			_modal.find( 'div.modal-body' )
+				.empty()
+				.append( render() );
+
+			_modal
+				.appendTo( 'body' )
+
+			_modal.addClass('is-active is-clipped');
+
+			$('.modal-close').one('click', function() {
+				_modal.removeClass('is-active is-clipped');
+			})
+			$('.modal-background').one('click', function() {
+				_modal.removeClass('is-active is-clipped');
+			})
+		}
 	};
 };
 
