8fc71ec25bce761fc0cb2c7030ca7d9dc2f0c7ab
SandyDatatables <sandy@datatables.net>
2021-05-24 11:00:14 +0000

    dev: Move type files to built folder during build

--- make.sh
+++ make.sh
@@ -35,6 +35,20 @@ rsync -r js $OUT_DIR
 js_compress $OUT_DIR/js/dataTables.responsive.js
 js_frameworks responsive $OUT_DIR/js
 
+# Copy Types
+if [ -d $OUT_DIR/types ]; then
+	rm -r $OUT_DIR/types		
+fi
+mkdir $OUT_DIR/types
+
+if [ -d types/ ]; then
+	cp types/* $OUT_DIR/types
+else
+	if [ -f types.d.ts ]; then
+		cp types.d.ts $OUT_DIR/types
+	fi
+fi
+
 # Copy and build examples
 rsync -r examples $OUT_DIR
 examples_process $OUT_DIR/examples
