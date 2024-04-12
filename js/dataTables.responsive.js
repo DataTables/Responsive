5cef001d85ad066031fb4febbbca666f9eadc533
Allan Jardine <allan@datatables.net>
2024-04-12 13:18:02 +0000

    Fix: Newly added rows did not have `dtr-hidden` class added to their cells if they were immediately hidden
    Fix: Responsive needs to call `-api columns.adjust()` if it has changed the column visibility. This is parimarily for scrolling so that the cloned header / footer elements can be updated.
    
    There is a warning to this action, since it would trigger Responsive's
    own resizing action on the `column-sizing` event listener. But there
    will be no change the second time around. So it isn't optimal, but
    unless we expose some kind of scroll draw option in DataTables, this is
    as close as we can get.

--- js/dataTables.responsive.js
+++ js/dataTables.responsive.js
@@ -146,7 +146,9 @@ $.extend(Responsive.prototype, {
 					var idx = dt.column.index('toData', i);
 
 					if (that.s.current[idx] === false) {
-						$(this).css('display', 'none');
+						$(this)
+							.css('display', 'none')
+							.addClass('dtr-hidden');
 					}
 				});
 			}
@@ -266,12 +268,6 @@ $.extend(Responsive.prototype, {
 
 			that._resizeAuto();
 			that._resize();
-
-			// If columns were hidden, then DataTables needs to adjust the
-			// column sizing
-			if ($.inArray(false, that.s.current)) {
-				dt.columns.adjust();
-			}
 		});
 
 		// First pass - draw the table for the current viewport size
@@ -998,6 +994,8 @@ $.extend(Responsive.prototype, {
 			});
 
 		if (changed) {
+			dt.columns.adjust();
+
 			this._redrawChildren();
 
 			// Inform listeners of the change
