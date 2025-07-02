ea09cf9c230cb6cae88cd07476af8abf86edb03e
Allan Jardine <allan@datatables.net>
2025-07-02 15:42:26 +0000

    Release 3.0.5

--- js/dataTables.responsive.js
+++ js/dataTables.responsive.js
@@ -1,11 +1,11 @@
-/*! Responsive 3.0.4
+/*! Responsive 3.0.5
  * © SpryMedia Ltd - datatables.net/license
  */
 
 /**
  * @summary     Responsive
  * @description Responsive tables plug-in for DataTables
- * @version     3.0.4
+ * @version     3.0.5
  * @author      SpryMedia Ltd
  * @copyright   SpryMedia Ltd.
  *
@@ -1789,7 +1789,7 @@ Api.registerPlural(
  * @name Responsive.version
  * @static
  */
-Responsive.version = '3.0.4';
+Responsive.version = '3.0.5';
 
 $.fn.dataTable.Responsive = Responsive;
 $.fn.DataTable.Responsive = Responsive;
