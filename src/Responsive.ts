f84541737ad8772e0acba4660dbdd7a8fcd32642
Allan Jardine <allan@datatables.net>
2026-07-17 07:23:07 +0000

    New: When a row's data is invalidated in DataTables core, its details display is now automatically udpated.

--- src/Responsive.ts
+++ src/Responsive.ts
@@ -152,6 +152,11 @@ export default class Responsive {
 			}
 		});
 
+
+		dt.on('rowInvalidate.dtr', (e, ctx, rowIdx) => {
+			this._redrawChildren();
+		});
+
 		// Destroy event handler
 		dt.on('destroy.dtr', function () {
 			dt.off('.dtr');
