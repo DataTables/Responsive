e4eadd8925633dcb5da1f60d364e8a0026a9e45a
Allan Jardine <allan@datatables.net>
2026-07-22 14:38:06 +0000

    Release 4.0.0 ready

--- src/Responsive.ts
+++ src/Responsive.ts
@@ -60,7 +60,7 @@ export default class Responsive {
 		tableAll: renderers.tableAll
 	}
 
-	public static version = '4.0.0-beta.1';
+	public static version = '4.0.0';
 
 	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 	 * Properties
